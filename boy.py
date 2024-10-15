3ba099dcb442939f9db48eea7d66e911f9787bb2
@TIP219 <>
2024-10-15 11:03:15 +0900

    refactor

--- boy.py
+++ boy.py
@@ -1,13 +1,18 @@
-class Idle:
-    @staticmethod
-    def enter(boy):
-        print('Boy Idle Enter')
-    @staticmethod
-    def exit(boy):
-        print('Boy Idle Exit')
-    @staticmethod
-    def do(boy):
-        boy.frame = (boy.frame + 1) % 8
-    @staticmethod
-    def draw(boy):
-        boy.image.clip_draw(boy.frame * 100, boy.action * 100, 100, 100, boy.x, boy.y)
+from pico2d import load_image
+
+class Boy:
+    def __init__(self):
+        self.x, self.y = 400, 90
+        self.frame = 0
+        self.dir = 0
+        self.action = 3
+        self.image = load_image('animation_sheet.png')
+
+    def update(self):
+        self.frame = (self.frame + 1) % 8
+
+    def handle_event(self, event):
+        pass
+
+    def draw(self):
+        self.image.clip_draw(self.frame * 100, self.action * 100, 100, 100, self.x, self.y)
