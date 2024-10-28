df255bb569739a620ca9685a14eac751b62a4dd6
@TIP219 <3dlstjd3@gmail.com>
2024-10-28 15:08:15 +0900

    월요일 수업 끝

--- boy.py
+++ boy.py
@@ -1,6 +1,7 @@
-from pico2d import load_image
+from pico2d import load_image, get_time
 
-from state_machine import StateMachine
+from state_machine import StateMachine, time_out, space_down
+
 
 class Boy:
     def __init__(self):
@@ -8,15 +9,69 @@ class Boy:
         self.frame = 0
         self.dir = 0
         self.action = 3
+        self.start_time = get_time()
         self.image = load_image('animation_sheet.png')
-        self.state_machine = StateMachine(self)
-        self.state_machine.start(Idle)
+        self.state_machine = StateMachine(self) # 소년 객체의 레퍼런스 전달
+        self.state_machine.start(Idle) # 초기상태 Idle
+        self.state_machine.set_transitions(
+            {
+                Idle : {time_out : Sleep},
+                Sleep: { space_down: Idle}
+            }
+        )
 
     def update(self):
-        self.frame = (self.frame + 1) % 8
+        self.state_machine.update()
 
     def handle_event(self, event):
-        pass
+        # event : 입력 이벤트 key mouse
+        # 우리가 전달할 내용은 튜플
+        self.state_machine.add_event(('INPUT', event))
 
     def draw(self):
-        self.image.clip_draw(self.frame * 100, self.action * 100, 100, 100, self.x, self.y)
+        self.state_machine.draw()
+        #self.image.clip_draw(self.frame * 100, self.action * 100, 100, 100, self.x, self.y)
+
+#상태를 클래스를 통해서 정의
+class Idle:
+    @staticmethod  # @ 데코레이터
+    def enter(boy):
+        #현재 시각 저장
+        boy.start_time = get_time()
+        pass
+
+    @staticmethod
+    def exit(boy):
+        pass
+
+    @staticmethod
+    def do(boy):
+        boy.frame = ( boy.frame + 1 ) % 8
+        if get_time() - boy.start_time > 3:
+            boy.state_machine.add_event(('TIME_OUT', 0))
+        pass
+
+    @staticmethod
+    def draw(boy):
+        boy.image.clip_draw(boy.frame * 100, boy.action * 100, 100, 100, boy.x, boy.y)
+
+
+class Sleep:
+    @staticmethod
+    def enter(boy):
+        pass
+
+    @staticmethod
+    def exit(boy):
+        pass
+
+    @staticmethod
+    def do(boy):
+        boy.frame = ( boy.frame + 1 ) % 8
+        pass
+
+    @staticmethod
+    def draw(boy):
+        boy.image.clip_composite_draw(
+            boy.frame * 100, boy.action * 100, 100, 100,
+            3.141592 / 2, '', boy.x - 25, boy.y - 25, 100, 100)
--- state_machine.py
+++ state_machine.py
@@ -1,9 +1,42 @@
+from tabnanny import check
+
+from sdl2 import SDL_KEYDOWN, SDLK_SPACE
+
+
 class StateMachine:
     def __init__(self, o):
-        pass
+        self.obj = o    # 어떤 객체를 위한 상태 머신인지 알려줌.
+        self.event_q = [] # 이벤트 보관 튜플 리스트(큐)
+    def add_event(self, e):
+        self.event_q.append(e)
+
     def start(self, state):
-        pass
+        self.cur_state = state
+
     def update(self):
-        pass
+        self.cur_state.do(self.obj)
+        # 혹시 이벤트가 있나
+        if self.event_q: #list는 멤버가 있으면 참 없으면 거짓
+            e = self.event_q.pop(0)
+            # 현재 상태와 현재 발생한 이벤트에 따라 다음 상태를 결정
+            # 상태 변환 테이블 활용
+            for check_event, next_state in self.transitions[self.cur_state].items():
+                if check_event(e): #
+                    self.cur_state.exit(self.obj)
+                    self.cur_state = next_state
+                    self.cur_state.enter(self.obj)
+
     def draw(self):
-        pass
+        self.cur_state.draw(self.obj)
+
+    def set_transitions(self, transitions):
+        self.transitions = transitions
+
+
+    #   이벤트 체크 함수
+    #상태 이벤트 e = (종류, 실제값) 튜플로 정의
+def space_down(e):
+    return e[0] == 'INPUT' and e[1].key == SDL_KEYDOWN and e[1].key == SDLK_SPACE
+
+def time_out(e):
+    return e[0] == 'TIME_OUT'
